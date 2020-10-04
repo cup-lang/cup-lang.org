ac8d8d572d4c8848d1b4affda973bf1eac8a53bb
Julek <31388661+jgrajewsky@users.noreply.github.com>
2020-10-04 20:55:02 +0200

    file size fetch

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function autorun() {
     fetch("https://api.github.com/repos/cup-lang/cup").then(res => res.json()).then(data => {
-        document.getElementById("star-count").innerHTML = '<svg viewBox="0 0 14 16" width="12" height="12" aria-hidden="true"><path fill-rule="evenodd"d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"></path></svg>' + data.stargazers_count;
+        document.getElementById("star-count").innerHTML = "<svg viewBox="0 0 14 16" width="12" height="12" aria-hidden="true"><path fill-rule="evenodd"d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"></path></svg>" + data.stargazers_count;
+    });
+
+    fetch("https://api.github.com/repos/cup-lang/cup/contents/bin").then(res => res.json()).then(data => {
+        document.getElementById("file-size").innerHTML = "v0.0.1 - " + data[0].size + "kB";
     });
 
     var platform = window.navigator.platform;
