23dfad90c04fb8d99678cf9e8ee6a6e2fd6876fb
Julek <31388661+jgrajewsky@users.noreply.github.com>
2020-10-04 20:56:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function autorun() {
     fetch("https://api.github.com/repos/cup-lang/cup").then(res => res.json()).then(data => {
-        document.getElementById("star-count").innerHTML = "<svg viewBox="0 0 14 16" width="12" height="12" aria-hidden="true"><path fill-rule="evenodd"d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"></path></svg>" + data.stargazers_count;
+        document.getElementById("star-count").innerHTML = '<svg viewBox="0 0 14 16" width="12" height="12" aria-hidden="true"><path fill-rule="evenodd"d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"></path></svg>' + data.stargazers_count;
     });
 
     fetch("https://api.github.com/repos/cup-lang/cup/contents/bin").then(res => res.json()).then(data => {
