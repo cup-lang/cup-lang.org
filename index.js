4d2dec480e7435452faf9f6583f30999a41383e6
Julek <31388661+jgrajewsky@users.noreply.github.com>
2020-10-06 12:14:50 +0200

    .

--- index.js
+++ index.js
@@ -45,7 +45,6 @@ function autorun() {
             window.onmousedown = hide;
             menu.style.display = "";
         }
-        return false;
     };
 
     fetch("https://api.github.com/repos/cup-lang/cup").then(res => res.json()).then(data => {
