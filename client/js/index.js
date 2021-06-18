ad7bd9c921fb037177231a4663f4f7cf42d43481
jgrajewsky <jgrajewsky@gmail.com>
2021-06-18 06:59:07 +0200

    .

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,17 @@
+Math.clamp = function (value, min, max) {
+    if (value < min) {
+        return min;
+    } else if (value > max) {
+        return max;
+    }
+    return value;
+};
+
+var logo = ['<svg ', 'id="logo" ', 'xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">', '<ellipse fill="#99AAB5" cx="18" cy="26" rx="18" ry="10"/><ellipse fill="#CCD6DD" cx="18" cy="24" rx="18" ry="10"/>', '<path fill="#F5F8FA" d="M18 31C3.042 31 1 16 1 12h34c0 2-1.958 19-17 19z"/><path fill="#CCD6DD" d="M34.385 9.644c2.442-10.123-9.781-7.706-12.204-5.799-1.34-.148-2.736-.234-4.181-.234-9.389 0-17 3.229-17 8.444C1 17.271 8.611 21.5 18 21.5s17-4.229 17-9.444c0-.863-.226-1.664-.615-2.412zm-2.503-2.692c-1.357-.938-3.102-1.694-5.121-2.25 1.875-.576 4.551-.309 5.121 2.25z"/><ellipse fill="#8A4B38" cx="18" cy="13" rx="15" ry="7"/><path fill="#D99E82" d="M20 17c-.256 0-.512-.098-.707-.293-2.337-2.337-2.376-4.885-.125-8.262.739-1.109.9-2.246.478-3.377-.461-1.236-1.438-1.996-1.731-2.077-.553 0-.958-.443-.958-.996 0-.552.491-.995 1.043-.995.997 0 2.395 1.153 3.183 2.625 1.034 1.933.91 4.039-.351 5.929-1.961 2.942-1.531 4.332-.125 5.738.391.391.391 1.023 0 1.414-.195.196-.451.294-.707.294zm-6-2c-.256 0-.512-.098-.707-.293-2.337-2.337-2.376-4.885-.125-8.262.727-1.091.893-2.083.494-2.947-.444-.961-1.431-1.469-1.684-1.499-.552 0-.989-.447-.989-1 0-.552.458-1 1.011-1 .997 0 2.585.974 3.36 2.423.481.899 1.052 2.761-.528 5.131-1.961 2.942-1.531 4.332-.125 5.738.391.391.391 1.023 0 1.414-.195.197-.451.295-.707.295z"/>', '</svg>'];
+logo.split = function () {
+    return logo[0] + logo[2] + logo[3] + logo[5] + logo[0] + 'id="cup-movable" ' + logo[2] + logo[4] + logo[5];
+}
+
 function updatePage(href) {
     var pages = document.querySelectorAll('body > div');
     for (let i = 0; i < pages.length; ++i) {
@@ -81,6 +95,8 @@ function setSize(size) {
 }
 
 function autorun() {
+    document.querySelector('[href="/"]').innerHTML = logo.join('') + 'Cup';
+
     document.querySelectorAll('.nav-link, .learn-link').forEach(function (e) {
         e.onclick = function () {
             history.pushState(null, '', e.href);
@@ -116,14 +132,14 @@ function autorun() {
             open = false;
             arrow.classList.remove('download-arrow-active');
             menu.style.display = 'none';
-            window.onmousedown = null;
+            window.removeEventListener('mousedown', hide);
         }
         if (open) {
             hide();
         } else {
             open = true;
             arrow.classList.add('download-arrow-active');
-            window.onmousedown = hide;
+            window.addEventListener('mousedown', hide);
             menu.style.display = '';
         }
         return false;
@@ -140,6 +156,53 @@ function autorun() {
         .then(function (data) {
             setSize(Math.floor(data[0].size / 1000));
         });
+
+    document.getElementById('cup').innerHTML = logo.split() + '<div id="cup-handle"></div>';
+    document.getElementById('cup-handle').onmousedown = function (e) {
+        e.stopPropagation();
+        var movable = document.getElementById('cup-movable');
+        function stopMoving() {
+            e.target.style = '';
+            window.removeEventListener('mouseup', stopMoving);
+            document.getElementById('cup').append(movable);
+            document.body.style = movable.style = '';
+            window.onmousemove = null;
+            clearInterval(interval);
+        }
+        window.addEventListener('mouseup', stopMoving);
+        e.target.style = 'display:none';
+        var rot = 0;
+        var x = e.pageX;
+        var y = e.pageY;
+        document.body.style = 'cursor:grabbing';
+        window.onmousemove = function (e) {
+            var newX = e.pageX;
+            rot = Math.clamp(rot + Math.clamp(newX - x, -10, 10), -140, 60);
+            x = newX;
+            y = e.pageY;
+        };
+        var time = Date.now();
+        var appended;
+        var interval = setInterval(function () {
+            if (!appended) {
+                document.body.append(movable);
+                appended = true;
+            }
+            var newTime = Date.now();
+            var delta = (newTime - time) / 2;
+
+            var t = -25 - rot;
+            var d = Math.clamp(t - Math.floor(t / 360) * 360, 0, 360);
+            if (d > 180) {
+                d -= 360;
+            }
+            rot = rot + d * Math.clamp(delta / 50, 0, 1);
+
+            movable.style = 'left:' + (x - 120) + ';top:' + (y - 22.5) + ';transform:rotate(' + rot + 'deg)';
+            time = newTime;
+        }, 0);
+        return false;
+    };
 }
 
 function onload() { }
--- server/static.js
+++ server/static.js
@@ -1,6 +1,5 @@
 const favicon = fs.readFileSync("assets/favicon.ico");
 const icon192 = fs.readFileSync("assets/icon192.png");
-const logo = fs.readFileSync("assets/logo.svg");
 const robots = fs.readFileSync("assets/robots.txt");
 
 const html = fs.readFileSync('build/out/client.html');
@@ -13,10 +12,6 @@ app.get('/favicon.ico', res => {
     res.writeHeader('Cache-Control', 'max-age=86400');
     res.writeHeader('Content-Type', 'image/png');
     res.end(icon192);
-}).get('/logo.svg', res => {
-    res.writeHeader('Cache-Control', 'max-age=86400');
-    res.writeHeader('Content-Type', 'image/svg+xml');
-    res.end(logo);
 }).get('/robots.txt', res => {
     res.writeHeader('Cache-Control', 'max-age=86400');
     res.writeHeader('Content-Type', 'text/plain');
