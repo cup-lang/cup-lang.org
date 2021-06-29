f657eb3758655c803ee91c622b5490f404c106f9
jgrajewsky <jgrajewsky@gmail.com>
2021-06-29 13:34:16 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.ws('/', {
         switch (type) {
             case 0:
                 fs.writeFileSync('playground/main.cp', data);
-                fs.writeFileSync('Dockerfile', 'FROM ubuntu\nWORKDIR /playground\nCOPY playground .\nCMD ./cup build -i prog');
+                fs.writeFileSync('Dockerfile', 'FROM ubuntu\nWORKDIR /playground\nCOPY playground .\nRUN chmod +x cup\nCMD ./cup build -i prog');
                 exec('docker build -t main . && docker run -t main', (err, stdout, stderr) => {
                     if (err) {
                         console.log(err);
