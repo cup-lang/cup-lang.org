95d63d42ec8fa5a899429f016487deb2bf016445
jgrajewsky <jgrajewsky@gmail.com>
2021-07-29 16:18:42 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -55,10 +55,10 @@ function checkQueue() {
         if (hash == null) {
             hash = hashFiles(req.files);
         }
-        // CLEANUP: clean if too many folders
+        // CLEANUP: clean cache if too big
         fs.mkdirSync(`playground/${hash}`);
         fs.writeFileSync(`playground/${hash}/main.cp`, req.files[0]);
-        exec(`echo "FROM ubuntu\nCOPY playground/cup .\nCOPY playground/${hash} prog/\nRUN chmod +x cup\nCMD ./cup build -i prog" | docker build -q -f - .`, (err, stdout) => {
+        exec(`echo "FROM ubuntu\nCOPY playground/cup .\nCOPY playground/${hash} prog/\nRUN chmod +x cup\nCMD ./cup build -i prog -o out.c && gcc out.c -o out && ./out" | docker build -q -f - .`, (err, stdout) => {
             if (err) {
                 return;
             }
