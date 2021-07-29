cfcb46babba7610ee5a26e1ce0629a96a8056b43
jgrajewsky <jgrajewsky@gmail.com>
2021-07-29 10:55:01 +0200

    docker test

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const uws = require('uWebSockets.js');
 embed('app.js');
 embed('static.js');
 
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 
 app.ws('/', {
     message: (ws, data) => {
@@ -13,15 +13,19 @@ app.ws('/', {
         switch (type) {
             case 0:
                 fs.writeFileSync('playground/prog/main.cp', data);
-                fs.writeFileSync('Dockerfile', 'FROM ubuntu\nWORKDIR /playground\nCOPY playground .\nRUN chmod +x cup\nCMD ./cup build -i prog');
-                exec('docker build -t main . > /dev/null && echo && docker run -t main', (err, stdout, stderr) => {
-                    if (err && err.code > 1) {
-                        return;
-                    }
-
-                    ws.send('\u0000' + stdout);
-                    ws.send('\u0001' + stderr);
+                const proc = spawn('docker', ['run', '--rm', '-it', '$(docker build -q -)']);
+                proc.stdout.on('data', function (data) {
+                    console.log(data);
                 });
+                proc.stdin.write('FROM ubuntu\nWORKDIR /playground\nCOPY playground .\nRUN chmod +x cup\nCMD ./cup build -i prog');
+                // exec('docker build -t main . > /dev/null && echo && docker run -t main', (err, stdout, stderr) => {
+                //     if (err && err.code > 1) {
+                //         return;
+                //     }
+
+                //     ws.send('\u0000' + stdout);
+                //     ws.send('\u0001' + stderr);
+                // });
                 break;
             default:
                 ws.close();
