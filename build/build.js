cc888ead1831265d172c2b7c81a1a9d8ef65b129
Julian Grajewski <me@jgrajewsky.com>
2022-02-25 13:13:21 +0100

    Add `postcss` and `autoprefixer` to build script

--- build/build.js
+++ build/build.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const htmlMinifier = require('html-minifier');
+const DEBUG = process.argv[2] == '--debug';
 
 function load(path) { return fs.readFileSync(path).toString(); }
 
-function embed(file) {
+async function embed(file) {
     for (let i = 0; i < file.length; ++i) {
         if (file.substr(i, 7) == 'embed(\'') {
             let filename = '';
@@ -13,27 +14,32 @@ function embed(file) {
                 }
                 filename += file[ii];
             }
-            let embeded = embed(load(filename));
-            if (!DEBUG && filename.endsWith('main.js')) {
-                embeded = `(() => {${embeded}})();`;
-                embeded = require('@babel/core').transformSync(embeded, { presets: ['@babel/preset-env'] }).code;
+            let embeded = await embed(load(filename));
+            if (!DEBUG) {
+                if (filename.endsWith('main.js')) {
+                    embeded = `(() => {${embeded}})();`;
+                    embeded = require('@babel/core').transformSync(embeded, { presets: ['@babel/preset-env'] }).code;
+                } else if (filename.endsWith('main.css')) {
+                    embeded = await require('postcss')([require('autoprefixer')]).process(embeded).css;
+                }
             }
+
             file = file.substr(0, i) + embeded + file.substr(i + 10 + filename.length);
         }
     }
     return file;
 }
 
-if (!fs.existsSync('build/out')) { fs.mkdirSync('build/out'); }
+(async function () {
+    if (!fs.existsSync('build/out')) { fs.mkdirSync('build/out'); }
 
-const DEBUG = process.argv[2] == '--debug';
+    // Build Client
+    let client = await embed(load('client/html/main.html'));
+    if (!DEBUG) {
+        client = htmlMinifier.minify(client, { minifyCSS: true, minifyJS: true, removeComments: true, sortClassName: true, sortAttributes: true, collapseWhitespace: true });
+    }
+    fs.writeFileSync('build/out/client.html', client);
 
-// Build Client
-let client = embed(load('client/html/main.html'));
-if (!DEBUG) {
-    client = htmlMinifier.minify(client, { minifyCSS: true, minifyJS: true, removeComments: true, sortClassName: true, sortAttributes: true, collapseWhitespace: true });
-}
-fs.writeFileSync('build/out/client.html', client);
-
-// Build Server
-fs.writeFileSync('build/out/server.js', embed(load('server/main.js')));
+    // Build Server
+    fs.writeFileSync('build/out/server.js', await embed(load('server/main.js')));
+})();
